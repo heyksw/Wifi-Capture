e6d972238611a56778d2879640c4ac4f7a8ba4f1
KimSangWoo <heyksw0208@naver.com>
2022-01-13 00:16:14 +0900

    UIImagePickerController photoLibrary 구현, handlePinch 2가지 state 나눠서 구현

--- Wifi-Capture/MainViewController.swift
+++ Wifi-Capture/MainViewController.swift
@@ -2,8 +2,16 @@ import SnapKit
 import Foundation
 import UIKit
 import AVFoundation
+import Photos
 
 class MainViewController: UIViewController {
+    
+    enum UserStates {
+        case beforeTakePictures
+        case afterTakePictures
+    }
+    
+    var userState: UserStates?
     var didSetupConstraints = false
     
     var captureDevice: AVCaptureDevice?
@@ -14,6 +22,11 @@ class MainViewController: UIViewController {
     var previewLayer: AVCaptureVideoPreviewLayer?
     let mainDispatchQueue = DispatchQueue.main
     let globalDispatchQueue = DispatchQueue.global()
+    
+    var recognizedPhotoScale: CGFloat = 1.0
+    let maxPhotoScale: CGFloat = 3.0
+    let minPhotoScale: CGFloat = 1.0
+     
     
     // safe area
     let safetyArea: UIView = {
@@ -102,16 +115,20 @@ class MainViewController: UIViewController {
         self.setUI()
         
         // 카메라 불러오기
-        self.settingCamera()
+        self.setCamera()
         
-        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(self.handlePinchCamera(_:)))
+        // 앨범에 접근할 권한 요청
+        self.getPhotoLibraryAuthorization()
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(self.handlePinch(_:)))
         view.addGestureRecognizer(pinch)
         cameraShootButton.addTarget(self, action: #selector(tapCameraShootButton(_:)), for: .touchDown)
+        galleryButton.addTarget(self, action: #selector(tapGalleryButton), for: .touchDown)
     }
     
     
     // AVFoundation camera setting
-    func settingCamera() {
+    func setCamera() {
         print("setting Camera")
         guard let captureDevice = getDefaultCamera() else {
             return
@@ -158,6 +175,13 @@ class MainViewController: UIViewController {
         
     }
     
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // user state 세팅
+        self.userState = .beforeTakePictures
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         globalDispatchQueue.async {
@@ -178,11 +202,11 @@ class MainViewController: UIViewController {
 }
 
 // 익스텐션
-extension MainViewController: AVCapturePhotoCaptureDelegate {
+extension MainViewController: AVCapturePhotoCaptureDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // 네비게이션 바 세팅
     func setUpNavigationBar() {
-        self.navigationItem.title = "Custom Camera"
+        self.navigationItem.title = "와캡"
         self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         self.navigationController?.navigationBar.tintColor = .white
         view.backgroundColor = .black
@@ -284,38 +308,66 @@ extension MainViewController: AVCapturePhotoCaptureDelegate {
     
     
     // 카메라 줌 인, 줌 아웃 구현
+    // 사진 줌 인, 줌 아웃 구현
     // device.videoZoomFactor 에 접근하려면 lock / unlock 과정이 필요함
     // https://developer.apple.com/documentation/avfoundation/avcapturedevice
     // https://gist.github.com/yusuke024/3b5a89835deab5b9027efea794b80a45
     @objc
-    func handlePinchCamera(_ pinch: UIPinchGestureRecognizer) {
-        guard let device = getDefaultCamera() else {return}
+    func handlePinch(_ pinch: UIPinchGestureRecognizer) {
         
-        var initialScale: CGFloat = device.videoZoomFactor
-        let minAvailableZoomScale = 1.0
-        let maxAvailableZoomScale = device.maxAvailableVideoZoomFactor
-        
-        do {
-            try device.lockForConfiguration()
-            if(pinch.state == UIPinchGestureRecognizer.State.began){
-                initialScale = device.videoZoomFactor
-            }
-            else {
-                if(initialScale*(pinch.scale) < minAvailableZoomScale){
-                    device.videoZoomFactor = minAvailableZoomScale
-                }
-                else if(initialScale*(pinch.scale) > maxAvailableZoomScale){
-                    device.videoZoomFactor = maxAvailableZoomScale
+        // 사진을 찍기 전, 그러니까 카메라가 실시간으로 보이고 있는 상황에서의 zoom 구현
+        if (self.userState == .beforeTakePictures) {
+            guard let device = getDefaultCamera() else {return}
+            
+            var initialScale: CGFloat = device.videoZoomFactor
+            let minAvailableZoomScale = 1.0
+            let maxAvailableZoomScale = device.maxAvailableVideoZoomFactor
+            
+            do {
+                try device.lockForConfiguration()
+                if(pinch.state == UIPinchGestureRecognizer.State.began){
+                    initialScale = device.videoZoomFactor
                 }
                 else {
-                    device.videoZoomFactor = initialScale * (pinch.scale)
+                    if(initialScale*(pinch.scale) < minAvailableZoomScale){
+                        device.videoZoomFactor = minAvailableZoomScale
+                    }
+                    else if(initialScale*(pinch.scale) > maxAvailableZoomScale){
+                        device.videoZoomFactor = maxAvailableZoomScale
+                    }
+                    else {
+                        device.videoZoomFactor = initialScale * (pinch.scale)
+                    }
                 }
+                pinch.scale = 1.0
+            } catch {
+                return
             }
-            pinch.scale = 1.0
-        } catch {
-            return
+            device.unlockForConfiguration()
         }
-        device.unlockForConfiguration()
+        // 사진을 찍은 후, 그러니까 사진의 결과가 카메라 뷰에 올라왔을 때의 zoom 구현
+        else if (self.userState == .afterTakePictures) {
+
+            mainDispatchQueue.async {
+                
+                if (pinch.state == .began || pinch.state == .changed){
+                    // 확대
+                    if(self.recognizedPhotoScale < self.maxPhotoScale && pinch.scale > 1.0){
+                        self.cameraView.transform = self.cameraView.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+                        self.recognizedPhotoScale *= pinch.scale
+                    }
+                    // 축소
+                    else if (self.recognizedPhotoScale > self.minPhotoScale && pinch.scale < 1.0) {
+                        self.cameraView.transform  = self.cameraView.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+                        self.recognizedPhotoScale *= pinch.scale
+                    }
+                }
+                pinch.scale = 1.0
+                print(self.recognizedPhotoScale)
+            }
+            
+        }
+        
     }
     
     
@@ -332,9 +384,27 @@ extension MainViewController: AVCapturePhotoCaptureDelegate {
     }
 
     
+    // 사진 앨범 접근 권한 요청
+    func getPhotoLibraryAuthorization() {
+        PHPhotoLibrary.requestAuthorization { status in
+            guard status == .authorized else {
+                print("not authorized error")
+                // 나중에 "앱을 다시 시작해보세요" 같은 에러 처리 구현해줘야겠다.
+                return
+            }
+        }
+    }
+    
+    
     // photoCapture proecess 가 끝날 떄 호출되는 delegate 메서드
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
         print("photoOutput")
+        
+        guard error == nil else {
+            print("Photo Output Error !")
+            return
+        }
+        
         guard let imageData = photo.fileDataRepresentation() else {
             print("imageData Error")
             return}
@@ -354,15 +424,54 @@ extension MainViewController: AVCapturePhotoCaptureDelegate {
             //self.cameraView.layer.removeFromSuperlayer()
             print("cameraView to outputImage")
             self.cameraView.layer.contents = outputImage
+            // 보내줄 때 scale 1로 초기화
+            self.recognizedPhotoScale = 1.0
         }
+        
+        // 사진을 찍으면 자동으로 저장
+        // Asynchronously runs a block that requests changes to the photo library.
+        PHPhotoLibrary.shared().performChanges({
+            let creationRequest = PHAssetCreationRequest.forAsset()
+            creationRequest.addResource(with: .photo, data: imageData, options: nil)
+        }, completionHandler: nil)
+        
+        userState = .afterTakePictures
+        
     }
 
     // 촬영 버튼 클릭 이벤트
     @IBAction func tapCameraShootButton(_ sender: UIButton) {
-        print("tapCameraShootButton")
         guard let setting = setting else {return}
         output?.capturePhoto(with: setting, delegate: self)
         
     }
-
+    
+    // 갤러리 버튼 클릭 이벤트 -> 앨범에 접근
+    // https://developer.apple.com/documentation/uikit/uiimagepickercontroller
+    @IBAction func tapGalleryButton(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        
+        guard UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) else {
+            print("can't use photo library")
+            return
+        }
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    // 앨범에서 사진을 선택한 뒤 실행되는 delegate 메서드
+    // https://developer.apple.com/documentation/uikit/uiimagepickercontrollerdelegate/1619126-imagepickercontroller
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            print("image Picked")
+            // 본격적 문자 인식
+            
+            
+        }
+    }
+    
 }
